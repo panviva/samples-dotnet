0ce162407d65478377a3c3b09c2f4ef22cabd624
Ravindu Nanayakkara <RavinduN@panviva.com>
2021-04-27 20:42:56 +1000

    Added copy to clipboard functionality.

--- Samples.Net.Artefacts.Web/Samples.Net.Artefacts.Web/ClientApp/src/components/search/SearchResultsListItem.js
+++ Samples.Net.Artefacts.Web/Samples.Net.Artefacts.Web/ClientApp/src/components/search/SearchResultsListItem.js
@@ -1,68 +1,102 @@
-import React from 'react';
-import { ListGroup, Card, Button, Badge } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import { ListGroup, Card, Button, Overlay, Toast } from 'react-bootstrap';
+import { Clipboard } from 'react-bootstrap-icons';
+import { toast } from 'react-toastify';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-export const SearchResultsListItem = ({ onMetaDataAdd, searchResults }) => (
-  <ListGroup variant="flush" defaultActiveKey={1}>
-    {searchResults?.results?.map((item) => {
-      const simpleContent = item.content
-        .filter((section) => section.mediaType === 'text/plain')
-        .map((section) => section.text)
-        .join(' ')
-        .trim();
+export const SearchResultsListItem = ({ onMetaDataAdd, searchResults }) => {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+  const notify = () => toast.success('Copied to clipboard!');
 
-      const metaDataBlock = (
-        <>
-          {Object.keys(item.metaData)
-            .filter((metaDatumName) => {
-              return item.metaData[metaDatumName].value;
-            })
-            .map((metaDatumName) => {
-              return (
-                <>
-                  {item.metaData[metaDatumName].value.map((metaDatumValue) => (
-                    <Button
-                      variant="secondary"
-                      style={{ margin: '5px' }}
-                      onClick={() =>
-                        onMetaDataAdd(metaDatumName, metaDatumValue)
-                      }
-                    >
-                      #{metaDatumValue}
-                    </Button>
-                  ))}
-                </>
-              );
-            })}
-        </>
-      );
+  const copyToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      toast.success('Copied to clipboard!');
+    } catch (err) {
+      toast.error('Could not copy text!');
+      console.error('Could not copy text: ', err);
+    }
+  };
 
-      const isLongAnswer = simpleContent.length > 50;
-      const cardTitle = isLongAnswer
-        ? simpleContent.slice(0, 49).trim().concat('...')
-        : simpleContent;
-      const cardText = isLongAnswer
-        ? '...'.concat(simpleContent.slice(49).trim())
-        : '';
-      const cardSubtitle = item.primaryQuery;
+  return (
+    <ListGroup variant="flush" defaultActiveKey={1}>
+      {searchResults?.results?.map((item) => {
+        const simpleContent = item.content
+          .filter((section) => section.mediaType === 'text/plain')
+          .map((section) => section.text)
+          .join(' ')
+          .trim();
 
-      return (
-        <React.Fragment key={item.id}>
-          <ListGroup.Item className="pl-2 m-2 search-results-list-item" action>
-            <Card>
-              <Card.Body>
-                <Card.Title>{cardTitle}</Card.Title>
-                <Card.Text>{cardText}</Card.Text>
-                <Card.Subtitle>{cardSubtitle}</Card.Subtitle>
-                <br />
-                {metaDataBlock}
-              </Card.Body>
-            </Card>
-          </ListGroup.Item>
-        </React.Fragment>
-      );
-    })}
-  </ListGroup>
-);
+        const metaDataBlock = (
+          <>
+            {Object.keys(item.metaData)
+              .filter((metaDatumName) => {
+                return item.metaData[metaDatumName].value;
+              })
+              .map((metaDatumName) => {
+                return (
+                  <>
+                    {item.metaData[metaDatumName].value.map(
+                      (metaDatumValue) => (
+                        <Button
+                          variant="secondary"
+                          style={{ margin: '5px' }}
+                          onClick={() =>
+                            onMetaDataAdd(metaDatumName, metaDatumValue)
+                          }
+                        >
+                          #{metaDatumValue}
+                        </Button>
+                      )
+                    )}
+                  </>
+                );
+              })}
+          </>
+        );
+
+        const isLongAnswer = simpleContent.length > 50;
+        const cardTitle = isLongAnswer
+          ? simpleContent.slice(0, 49).trim().concat('...')
+          : simpleContent;
+        const cardText = isLongAnswer
+          ? '...'.concat(simpleContent.slice(49).trim())
+          : '';
+        const cardSubtitle = item.primaryQuery;
+
+        return (
+          <React.Fragment key={item.id}>
+            <ListGroup.Item
+              className="pl-2 m-2 search-results-list-item"
+              action
+            >
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    <span style={{ marginRight: '0.5em' }}>
+                      <Button
+                        variant="secondary"
+                        ref={target}
+                        onClick={() => copyToClipboard(simpleContent)}
+                      >
+                        <Clipboard />
+                      </Button>
+                    </span>
+                    {cardTitle}
+                  </Card.Title>
+                  <Card.Text>{cardText}</Card.Text>
+                  <Card.Subtitle>{cardSubtitle}</Card.Subtitle>
+                  <br />
+                  {metaDataBlock}
+                </Card.Body>
+              </Card>
+            </ListGroup.Item>
+          </React.Fragment>
+        );
+      })}
+    </ListGroup>
+  );
+};
--- Samples.Net.Artefacts.Web/Samples.Net.Artefacts.Web/ClientApp/src/index.js
+++ Samples.Net.Artefacts.Web/Samples.Net.Artefacts.Web/ClientApp/src/index.js
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -11,6 +13,18 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(
   <BrowserRouter basename={baseUrl} forceRefresh={true}>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={1500}
+      hideProgressBar={true}
+      newestOnTop={true}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+    {/* Same as */}
   </BrowserRouter>,
   rootElement
 );
